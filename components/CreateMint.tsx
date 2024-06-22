47c33794fa76c88c4bd696e3f9703b8b0a945286
Justin Mendonca <j.p.moregain15@gmail.com>
2024-06-22 18:13:11 -0400

    Build out CreateMint function

--- components/CreateMint.tsx
+++ components/CreateMint.tsx
@@ -1,24 +1,24 @@
-import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import * as web3 from "@solana/web3.js";
-import { FC, useState } from "react";
-import styles from "../styles/Home.module.css";
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import * as web3 from '@solana/web3.js';
+import { FC, useState } from 'react';
+import styles from '../styles/Home.module.css';
 import {
   MINT_SIZE,
   TOKEN_PROGRAM_ID,
   getMinimumBalanceForRentExemptMint,
   createInitializeMintInstruction,
-} from "@solana/spl-token";
+} from '@solana/spl-token';
 
 export const CreateMintForm: FC = () => {
-  const [txSig, setTxSig] = useState("");
-  const [mint, setMint] = useState("");
+  const [txSig, setTxSig] = useState('');
+  const [mint, setMint] = useState('');
 
   const { connection } = useConnection();
   const { publicKey, sendTransaction } = useWallet();
   const link = () => {
     return txSig
       ? `https://explorer.solana.com/tx/${txSig}?cluster=devnet`
-      : "";
+      : '';
   };
 
   const createMint = async (event) => {
@@ -27,7 +27,29 @@ export const CreateMintForm: FC = () => {
       return;
     }
 
-    // BUILD AND SEND CREATE MINT TRANSACTION HERE
+    const accountKeypair = web3.Keypair.generate()
+    const lamports = await getMinimumBalanceForRentExemptMint(connection)
+
+    const transaction = new web3.Transaction().add(
+      web3.SystemProgram.createAccount({
+        fromPubkey: publicKey,
+        newAccountPubkey: accountKeypair.publicKey,
+        space: MINT_SIZE,
+        lamports,
+        programId: TOKEN_PROGRAM_ID,
+      }),
+      createInitializeMintInstruction(
+        accountKeypair.publicKey,
+        8,
+        publicKey,
+        publicKey,
+        TOKEN_PROGRAM_ID
+      )
+    );
+
+    const tx = await sendTransaction(transaction, connection, { signers: [accountKeypair]})
+    setTxSig(tx)
+    setMint(accountKeypair.publicKey.toString())
   };
 
   return (
